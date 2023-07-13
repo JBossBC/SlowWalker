20619da6279207f8587fb36c5b8913cf3c05143f
xiyang <1577002722@qq.com>
2023-07-13 22:46:49 +0800

    commit the configs

---
+++ deployments/pipeline.groovy
@@ -0,0 +1,49 @@
+pipeline{
+    agent any
+    environment {
+        // 设置Go环境变量
+        GO_HOME = "/usr/local/go"
+        PATH = "${GO_HOME}/bin:${PATH}"
+    }
+    stages{
+        stage("pull"){
+            steps{
+                git branch: 'main', url: 'https://github.com/JBossBC/repliteWeb.git'
+            }
+        }
+         stage('Set golang proxy') {
+            steps {
+                // 设置Go代理
+                script {
+                    env.GO111MODULE = 'on'
+                    env.GOPROXY = 'https://goproxy.cn,direct'
+                }
+            }
+        }
+        // stage("test"){
+        //     steps{
+        //         sh 
+        //     }
+        // }
+        stage("build"){
+            steps{
+                sh 'go build -o backend ./cmd/main.go'
+                withCredentials([sshUserPrivateKey(credentialsId: 'jenkins', keyFileVariable: 'SSH_KEY')]) {
+                    // 通过SCP命令将文件复制到节点
+                    //TODO should use node to connect with ip
+                    sh """
+                       ssh-agent bash -c 'ssh-add $SSH_KEY; scp ./configs/ root@112.124.53.234:/opt'
+                        ssh-agent bash -c 'ssh-add $SSH_KEY; scp backend root@112.124.53.234:/opt'
+                    """
+                } 
+            }
+        }
+        stage("report"){
+            steps{
+               node("backend"){
+                 sh './backend'
+               }
+            }
+        }
+    }
+}
