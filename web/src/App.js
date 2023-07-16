e2f02f0a5b74a8d6692c2edafe9b9672c1043631
xiyang <1577002722@qq.com>
2023-07-16 21:56:29 +0800

    create the main html template

--- web/src/App.js
+++ web/src/App.js
@@ -2,19 +2,20 @@ import {BrowserRouter as Router,Route,Routes}  from "react-router-dom"
 import './App.css';
 
 import React from "react";
-import { NotFound,Index,Register } from "./components";
-const backend = React.createContext("http://localhost:8080/");
+import { NotFound,Index,Register, Main } from "./components";
+const backend = React.createContext("http://112.124.53.234:8080/");
 function App(){
   return(
     <Router>
+     <backend.Provider>
       <Routes>
-        <backend.Provider>
       <Route path="/" Component={Index}/>
       <Route path="/login" Component={Index}/>
       <Route path="/register" Component={Register}></Route>
+      <Route path="/main" Component={Main}></Route>
        <Route path="*" Component={NotFound}/>
-       </backend.Provider>
        </Routes>
+       </backend.Provider>
     </Router>
   )
 }
--- web/src/components/index.js
+++ web/src/components/index.js
@@ -4,3 +4,8 @@ export {default as NotFound} from './NotFound'
 export {default as Index} from "./Login"
 
 export {default as Register} from "./Register"
+
+
+export {default as Main} from "./Main.jsx"
+
+export {default as Log} from "./Log"
