ddc2a2f55321578c0afcd05f84dc1d6b3523b25f
xiyang <1577002722@qq.com>
2023-08-29 16:25:13 +0800

    require the user_test

--- test/user_test.go
+++ test/user_test.go
@@ -13,7 +13,7 @@ func TestLogin(t *testing.T) {
 		Username: "member",
 		Password: "member",
 	}
-	response, jwtStr := service.LoginAccount(user)
+	response, jwtStr := service.GetUserService().LoginAccount(user)
 	expectResponse := utils.NewSuccessResponse("登录成功")
 	if len(jwtStr) == 0 {
 		t.Errorf("返回的jwtStr为空")
@@ -30,7 +30,7 @@ func TestCreateAccount(t *testing.T) {
 		PhoneNumber: "1234567890",
 		Code:        "123",
 	}
-	response := service.CreateAccount(User)
+	response := service.GetUserService().CreateAccount(User)
 	Response1 := utils.NewFailedResponse("注册次数太多,请等一会再试")
 	Response2 := utils.NewFailedResponse("系统错误")
 	Response3 := utils.NewFailedResponse("用户名已存在")
@@ -60,7 +60,7 @@ func TestUpdateInfo(t *testing.T) {
 		Password:    "member",
 		PhoneNumber: "15182242848",
 	}
-	response := service.UpdateInfo(User)
+	response := service.GetUserService().UpdateInfo(User)
 	Response1 := utils.NewFailedResponse("系统错误")
 	Response2 := utils.NewFailedResponse("修改的用户信息不存在")
 	Response3 := utils.NewFailedResponse("创建失败")
@@ -79,7 +79,7 @@ func TestQueryUser(t *testing.T) {
 	User := &dao.User{
 		Username: "member",
 	}
-	response := service.QueryUser(User)
+	response := service.GetUserService().QueryUser(User)
 	response1 := utils.NewFailedResponse("系统错误")
 	if response == response1 {
 		t.Fatalf("系统错误")
@@ -87,7 +87,7 @@ func TestQueryUser(t *testing.T) {
 	log.Println(response)
 }
 func TestQueryUsers(t *testing.T) {
-	response := service.QueryUsers(1, 1)
+	response := service.GetUserService().QueryUsers(1, 1)
 	response1 := utils.NewFailedResponse("查询失败")
 	if response == response1 {
 		t.Fatalf("查询失败")
@@ -98,7 +98,7 @@ func TestDeleteUser(t *testing.T) {
 	user := &dao.User{
 		Username: "memberafafds",
 	}
-	response := service.DeleteUser(user)
+	response := service.GetUserService().DeleteUser(user)
 	response1 := utils.NewFailedResponse("删除失败")
 	if response == response1 {
 		t.Fatalf("删除失败")
